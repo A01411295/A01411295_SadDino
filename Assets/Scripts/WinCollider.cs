33942c93f36186421ccc8b5b175304c1a2e63367
A01411295 <A01411295@itesm.mx>
2018-03-11 18:02:32 -0600

    game

---
+++ Assets/Scripts/WinCollider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCollider : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        LevelManager levelManager = new LevelManager();
+        levelManager.LoadLevel("Win");
+    }
+}
